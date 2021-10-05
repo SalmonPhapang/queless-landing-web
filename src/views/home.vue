bdd712b55d36d2aa3503746ee61252ea3e76edbe
Salmon <salmon@anylytical.co.za>
2021-10-05 17:29:32 +0200

    added cookie consent on website

--- src/views/home.vue
+++ src/views/home.vue
@@ -17,7 +17,6 @@
     </div>
   </div>
 
-
   <header class="header">
     <div class="navbar-area">
       <div class="container">
@@ -473,15 +472,78 @@
         </div>
       </div>
     </div>
+
+    <div class="hello fixed-bottom p-4">
+      <div class="bg-dark text-white w-100 mw-100">
+        <div class="toast-body p-4 d-flex flex-column">
+          <h4 class="text-white">Cookie Warning</h4>
+          <p>
+            This website stores data such as cookies to enable site functionality including analytics and personalization. By using this website, you automatically accept that we use cookies.
+          </p>
+          <div class="ml-auto">
+            <button type="button" class="btn btn-outline-light mr-3" id="btnDeny">
+              Deny
+            </button>
+            <button type="button" class="btn btn-light" id="btnAccept">
+              Accept
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
   </footer>
-
-
   <a href="#" class="scroll-top btn-hover">
     <i class="lni lni-chevron-up"></i>
   </a>
 </template>
 <script>
+import * as $ from 'jquery';
 export default {
-  name: "home"
+  name: "home",
+  mounted: function() {
+    function setCookie(name,value,days) {
+      var expires = "";
+      if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+      }
+      document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+    }
+    function getCookie(name) {
+      var nameEQ = name + "=";
+      console.log(document.cookie);
+      var ca = document.cookie.split(';');
+      for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+      }
+      return null;
+    }
+
+    function eraseCookie(name) {
+      document.cookie = name +'=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+    }
+
+    function cookieConsent() {
+      if (!getCookie('qlessAllowCookies')) {
+        $('.hello').show();
+      }
+    }
+
+    $('#btnDeny').click(()=>{
+      eraseCookie('qlessAllowCookies')
+      $('.hello').hide();
+    })
+
+    $('#btnAccept').click(()=>{
+      setCookie('qlessAllowCookies','1',7)
+      $('.hello').hide();
+    });
+
+    cookieConsent();
+  }
 }
+
 </script>
